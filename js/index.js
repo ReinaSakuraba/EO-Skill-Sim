bdd7f605dadc1be006c2c507c21c3cd7437fef22
ReinaSakuraba <juniorterra7@gmail.com>
2020-08-30 22:49:11 -0700

    Fix dynamic import problem

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const gameRegex = new RegExp('^(?:\\/.+?)*?\\/([a-z]+)\\/(?:index\\.html)?$');
   const game = window.location.pathname.match(gameRegex)[1];
 
-  const {Simulator: Sim, forward, levels, skills, secondaryPenalty, retireBonuses, levelCaps} = await import(`../data/${game}/`);
+  const {Simulator: Sim, forward, levels, skills, secondaryPenalty, retireBonuses, levelCaps} = await import(`../data/${game}/index.js`);
 
   simulator = new Sim({forward, levels, skills, secondaryPenalty, retireBonuses, levelCaps});
 });
